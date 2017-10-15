8bbe08ce92409d10014d18fe54183bbe4bdae70e
Kajetan <kajetan@student.edu>
2017-10-15 21:52:20 +0200

    first commit

---
+++ magento/app/etc/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_32bd54',
+  ),
+  'crypt' => 
+  array (
+    'key' => '72c5fcdb75da8b89b88b64e1646617d7',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento',
+        'username' => 'root',
+        'password' => 'coderslab',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'full_page' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 26 Sep 2017 18:33:33 +0000',
+  ),
+);
